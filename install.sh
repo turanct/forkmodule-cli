8138d71c76e2f32e78565e94b8026071ee707da5
Toon Daelman <toon@sumocoders.be>
2013-11-01 18:09:40 +0100

    Add installer

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Get ENV variable
+PREFIX=${PREFIX:='/usr/local'}
+
+# Clone the git repo
+git clone https://github.com/turanct/forkmodule-cli.git $PREFIX/forkmodule-cli
+
+# cd to the forkmodule-cli directory
+cd $PREFIX/forkmodule-cli
+
+# Get composer
+curl -sS https://getcomposer.org/installer | php
+
+# Install dependencies using composer
+php composer.phar install
+
+# Symlink to $PREFIX/bin
+ln -s $PREFIX/forkmodule-cli/forkmodule.php $PREFIX/bin/forkmodule
