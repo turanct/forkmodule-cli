26e2801b3f98415b27367973b9b07962fa9fefc5
turanct <spinnewebber_toon@hotmail.com>
2013-12-29 00:22:13 +0100

    Don't create a backend delete action template
    Fixes #12

--- src/Forkmodule/Backend/Action.php
+++ src/Forkmodule/Backend/Action.php
@@ -14,8 +14,10 @@ class Action extends Forkcontroller {
 		$content = $this->app['twig']->render('backend.actions.index.php', array('moduleName' => $this->app['module.name'], 'action' => $this->name));
 		file_put_contents($this->app['module.dir.backend'] . 'actions/'.$this->name.'.php', $content);
 
-		$templateFile = ($this->name == 'index') ? 'index' : 'action';
-		$content = $this->app['twig']->render('backend.layout.templates.'.$templateFile.'.tpl', array('moduleName' => $this->app['module.name'], 'action' => $this->name));
-		file_put_contents($this->app['module.dir.backend'] . 'layout/templates/'.$this->name.'.tpl', $content);
+		if ($this->name != 'delete') {
+			$templateFile = ($this->name == 'index') ? 'index' : 'action';
+			$content = $this->app['twig']->render('backend.layout.templates.'.$templateFile.'.tpl', array('moduleName' => $this->app['module.name'], 'action' => $this->name));
+			file_put_contents($this->app['module.dir.backend'] . 'layout/templates/'.$this->name.'.tpl', $content);
+		}
 	}
 }
