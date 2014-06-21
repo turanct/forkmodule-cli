270792174df9558bb3e0a29dbec8780472bc2c26
turanct <spinnewebber_toon@hotmail.com>
2014-06-21 13:20:53 +0200

    Fix comment indentation

--- src/Forkmodule/Service/Update.php
+++ src/Forkmodule/Service/Update.php
@@ -7,13 +7,13 @@ namespace Forkmodule\Service;
  */
 class Update
 {
-	/**
-	 * Run method
-	 *
-	 * This methods runs an update on the repository
-	 *
-	 * @param string $basedir The basedir of the forkmodule installation
-	 */
+    /**
+     * Run method
+     *
+     * This methods runs an update on the repository
+     *
+     * @param string $basedir The basedir of the forkmodule installation
+     */
     public function run($basedir)
     {
         passthru('cd ' . realpath($basedir) . ' && git pull origin master && git remote update && cd -');
